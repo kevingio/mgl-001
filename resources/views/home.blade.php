d76f01ee7b2987b2edea069edaf9f45a3d8d7647
Kevin Giovanni <kevin.giovanni@ti.ukdw.ac.id>
2019-06-04 12:36:05 +0700

    http upgrade insecure requests

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="description" content="Aplikasi manajemen inventaris berbasis web">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- resources/views/modules/laravelpwa/meta.blade.php
+++ resources/views/modules/laravelpwa/meta.blade.php
@@ -1,5 +1,5 @@
 <!-- Web Application Manifest -->
-<link rel="manifest" href="{{ asset(route('laravelpwa.manifest'), true) }}">
+<link rel="manifest" href="{{ route('laravelpwa.manifest') }}">
 <!-- Chrome for Android theme color -->
 <!-- <meta name="theme-color" content="{{ $config['theme_color'] }}"> -->
 
